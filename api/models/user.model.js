38ed8161879cac128bed846afc02fba41aa036c6
abhikumaryadav <144552283+abhikumaryadav@users.noreply.github.com>
2024-06-02 10:55:11 +0530

    Add files via upload

---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },  
+    avatar: {
+        type: String,
+        default: "https://th.bing.com/th/id/OIP.GHGGLYe7gDfZUzF_tElxiQHaHa?rs=1&pid=ImgDetMain"
+
+    } 
+
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
